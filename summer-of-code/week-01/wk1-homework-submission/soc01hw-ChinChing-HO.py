f508d3ee0dabf1f983927530363147aa9e03942f
originaljplus <41297205+originaljplus@users.noreply.github.com>
2018-07-20 00:33:53 +0930

    Update soc01hw-ChinChing-HO.py

--- summer-of-code/week-01/wk1-homework-submission/soc01hw-ChinChing-HO.py
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-ChinChing-HO.py
@@ -1,1 +1,41 @@
+#Day1 [16/7/2018]
+#A Few Things to Try
+#1. Hours in a year. How many hours are in a year?
+print(24*365)
+print(24*366)
+# If that year is a normal year with 365 days, there are 8760 hours in a year.
+# If that year is a leap year with 366 days, there are 8784 hours in a year.
 
+#2.Minutes in a decade. How many minutes are in a decade?
+print(60*24*365*10)
+# there are 5256000 minutes in a decade.
+
+#3.Your age in seconds. How many seconds old are you?
+print(20*60*60*24*365)
+#I am 630720000 seconds old.
+
+#4.Andreea Visanoiu: I'm 486180000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+print(486180000/60/60/24/365)
+#Andreea is 15 years old.
+
+#Here are some tougher questions:
+#5.How many days does it take for a 32-bit system to timeout, if it has a bug with integer overflow?
+#Integer n = 1;
+print("infinte")
+
+#6.How about a 64-bit system?
+print()
+
+#7.Calculate your age accurately based on your birthday (maybe use time of day e.g. 8:23am if you know it, use 12:00 noon midday)
+
+#Day2 [17/7/2018]
+#No Homework 
+
+#Day3 [18/7/2018]
+#1.Full name greeting. Write a program that asks for a person’s first name, then middle, and then last. Finally, it should greet the person using their full name.
+
+#2.Bigger, better favorite number. Write a program that asks for a person’s favorite number. Have your program add 1 to the number, and then suggest the result as a bigger and better favorite number. (Do be tactful about it, though.)
+
+#Day4 [19/7/2018]
+
+#Day5 [20/7/2018]
